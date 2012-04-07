50c2f2fc859b43df8560cf2890fb871d275934e6
Pankaj Kumar <me@panks.in>
2012-04-07 06:16:30 +0530

    Added eveything for first push

---
+++ src/com/app/reminderapp/RemSvcAutoStarter.java
@@ -0,0 +1,21 @@
+package com.app.reminderapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class RemSvcAutoStarter extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context ctx, Intent i) {
+		// TODO Auto-generated method stub
+		Log.e("RemSvcAutoStarter", "Received intent" + i.toString());
+		Intent serviceIntent = new Intent();
+		serviceIntent.setAction("com.app.placeme.ReminderService");
+		ctx.startService(serviceIntent);
+
+		
+	}
+
+}
